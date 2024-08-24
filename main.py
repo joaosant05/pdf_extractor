04e8a30195f2ae001f650f83e8d3386a5c38b707
João Santos <joaogabriel.jog220@gmail.com>
2024-08-24 14:49:32 -0300

    update to extract tables
    in this update i added function to extract table from pages, initially i use camelot and tabulate, butt after some problems I decidet to use pdfplumber, now i can extract tables.
    
    For next step i want to configurate format from tables to extract in layout i want

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-import fitz  # PyMuPDF
+import fitz
 import os
 import hashlib
 from PIL import Image, ImageOps
 import io
 import numpy as np
+import pdfplumber
+import pandas as pd
 
 def is_black_background(img):
-    # Converte a imagem para escala de cinza
     img_gray = img.convert('L')
     img_array = np.array(img_gray)
     
-    # Calcula a proporção de pixels pretos (valores próximos de 0)
-    black_pixels = np.sum(img_array < 50)  # Ajuste o limiar conforme necessário
+    black_pixels = np.sum(img_array < 50)
     total_pixels = img_array.size
     black_ratio = black_pixels / total_pixels
     
-    return black_ratio > 0.8  # Ajuste a proporção conforme necessário
+    return black_ratio > 0.8
 
 def invert_colors(img):
-    # Inverte as cores da imagem
     img_inverted = ImageOps.invert(img.convert('RGB'))
     return img_inverted
 
@@ -27,23 +26,41 @@ def save_image_as_png(image_bytes, output_path):
         if img.mode not in ['RGB', 'RGBA']:
             img = img.convert('RGB')
         
-        # Verifica se a imagem tem fundo preto e inverte as cores se necessário
         if is_black_background(img):
             img = invert_colors(img)
         
         img.save(output_path, format="PNG")
 
+def extract_tables_with_pdfplumber(pdf_path, output_folder, start_page, num_pages):
+    tables_output_folder = os.path.join(output_folder, "tables")
+
+    if not os.path.exists(tables_output_folder):
+        os.makedirs(tables_output_folder)
+
+    with pdfplumber.open(pdf_path) as pdf:
+        for page_num in range(start_page, start_page + num_pages):
+            page = pdf.pages[page_num]
+            tables = page.extract_tables()
+
+            if tables:
+                for i, table in enumerate(tables):
+                    df = pd.DataFrame(table)
+                    excel_filename = f"{tables_output_folder}/page{page_num+1}_table{i+1}.xlsx"
+                    df.to_excel(excel_filename, index=False)
+                    print(f"Table on page {page_num+1} saved as {excel_filename}")
+            else:
+                print(f"No tables found on page {page_num+1}.")
+
 def extract_main_image_from_pdf(pdf_path, output_folder, start_page, num_pages):
     pdf = fitz.open(pdf_path)
-    saved_hashes = set()  # Conjunto para armazenar hashes das imagens salvas
+    saved_hashes = set()
     
-    # Loop para ler páginas
-    for page_num in range(start_page, (start_page + 1) + num_pages):
+    for page_num in range(start_page, start_page + num_pages):
         page = pdf.load_page(page_num)
         images = page.get_images(full=True)
         
         main_image = None
-        max_area = 0  # Prioriza a maior imagem
+        max_area = 0
         
         for img_index, img in enumerate(images):
             xref = img[0]
@@ -61,17 +78,13 @@ def extract_main_image_from_pdf(pdf_path, output_folder, start_page, num_pages):
         if main_image:
             image_bytes, image_ext, page_num, img_index = main_image
             
-            # Calcula o hash SHA-256 da imagem
             image_hash = hashlib.sha256(image_bytes).hexdigest()
             
-            # Verifica se o hash da imagem já está no conjunto
             if image_hash not in saved_hashes:
-                # Converte qualquer imagem para PNG
                 image_filename = f"{output_folder}/page{page_num+1}_img{img_index+1}.png"
                 save_image_as_png(image_bytes, image_filename)
                 
                 print(f"Main image on page {page_num+1} saved as {image_filename}")
-                # Adiciona o hash da imagem ao conjunto
                 saved_hashes.add(image_hash)
             else:
                 print(f"Duplicate image on page {page_num+1} skipped.")
@@ -80,7 +93,7 @@ def extract_main_image_from_pdf(pdf_path, output_folder, start_page, num_pages):
 
 # Exemplo de uso
 pdf_path = "teste_2.pdf"
-output_folder = "images"
+output_folder = "output"
 start_page = int(input("Enter the start page (index starting from 0): "))
 num_pages = int(input("Enter the number of pages to extract: "))
 
@@ -88,3 +101,4 @@ if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
 extract_main_image_from_pdf(pdf_path, output_folder, start_page, num_pages)
+extract_tables_with_pdfplumber(pdf_path, output_folder, start_page, num_pages)
